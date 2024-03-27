d34bbc1fb2ba5bac24e7030d94df27137706883a
MatoG50 <matog50@gmail.com>
2024-03-27 22:36:24 +0300

    initial details page

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,12 +1,16 @@
 import { HStack, Text, useColorMode } from '@chakra-ui/react';
-import { IoMoonOutline, IoMoonSharp } from 'react-icons/io5';
+import { IoMoonSharp } from 'react-icons/io5';
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <HStack onClick={toggleColorMode} className='switch'>
-      {colorMode === 'dark' ? <IoMoonOutline /> : <IoMoonSharp />}
+      {colorMode === 'dark' ? (
+        <IoMoonSharp style={{ color: 'white' }} />
+      ) : (
+        <IoMoonSharp />
+      )}
       {colorMode === 'dark' ? <Text>Light Mode</Text> : <Text>Dark Mode</Text>}
     </HStack>
   );
