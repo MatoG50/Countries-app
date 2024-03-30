9bffe92fc2020f2415172e3f6649d9c4cd7a23e2
MatoG50 <matog50@gmail.com>
2024-03-30 19:06:46 +0300

    responsive ui

--- src/pages/CountryDetailPage.tsx
+++ src/pages/CountryDetailPage.tsx
@@ -1,14 +1,6 @@
 import { Link, useParams } from 'react-router-dom';
 import useCountry from '../hooks/useCountry';
-import {
-  Image,
-  Heading,
-  Text,
-  Box,
-  Card,
-  Flex,
-  Container,
-} from '@chakra-ui/react';
+import { Image, Heading, Text, Box, Card, Flex } from '@chakra-ui/react';
 import { IoIosArrowRoundBack } from 'react-icons/io';
 
 const CountryDetailPage = () => {
@@ -25,7 +17,7 @@ const CountryDetailPage = () => {
 
   return (
     <>
-      <Container maxW='container.lg'>
+      <div style={{ width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
         <Link to='/'>
           <Flex
             alignItems='center'
@@ -42,77 +34,106 @@ const CountryDetailPage = () => {
             Back
           </Flex>
         </Link>
-        <Flex></Flex>
-        <Card width='500px' padding='0'>
-          <Image
-            borderRadius='md'
-            width='100%'
-            src={data[0].flags.png}
-            alt={data[0].flags.alt}
-          />
-        </Card>{' '}
-        <Heading> {data[0].name.common}</Heading>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Native Name:</Text>
-          <Text ml='2'>
-            {
-              data[0].name.nativeName[Object.keys(data[0].name.nativeName)[0]]
-                .common
-            }
-          </Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Population:</Text>
-          <Text ml='2'>{data[0].population}</Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Region:</Text>
-          <Text ml='2'>{data[0].region}</Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Sub Region:</Text>
-          <Text ml='2'>{data[0].subregion}</Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Capital:</Text>
-          <Text ml='2'>{data[0].capital}</Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Top Level Domain:</Text>
-          <Text ml='2'>{data[0].demonyms.eng?.m}</Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Currencies:</Text>
-          <Text ml='2'>
-            {data[0].currencies[Object.keys(data[0].currencies)[0]].name}
-          </Text>
-        </Flex>
-        <Flex alignItems='baseline'>
-          <Text fontWeight='bold'>Languages:</Text>
-          <Text ml='2'>{Object.values(data[0].languages).join(', ')}</Text>
-        </Flex>
-        <Flex>
-          {data[0].borders && <Text fontWeight='bold'>Border countries: </Text>}
-          <Flex ml='4' flexWrap='wrap'>
-            {data[0].borders &&
-              data[0].borders.map((border: string) => (
-                <Box
-                  borderRadius='md'
-                  key={border}
-                  boxShadow='md'
-                  w='78px'
-                  h='36px'
-                  paddingTop='5px'
-                  paddingBottom='5px'
-                  paddingLeft='20px'
-                  paddingRight='20px'
-                >
-                  {border}
-                </Box>
-              ))}
-          </Flex>
-        </Flex>
-      </Container>
+        <div className='column'>
+          <div className='left-side'>
+            <Card
+              padding='0'
+              marginRight='30px'
+              width={{ base: '100%', md: '350px', lg: '500px' }}
+              height={{ base: 'auto', md: '240px', lg: '350px' }}
+            >
+              <Image
+                width={{ base: '100%', md: '350px', lg: '500px' }}
+                height={{ base: 'auto', md: '240px', lg: '350px' }}
+                borderRadius='md'
+                src={data[0].flags.png}
+                alt={data[0].flags.alt}
+              />
+            </Card>{' '}
+          </div>
+          <div className='right-side'>
+            <Heading className='txt' marginBottom='40px'>
+              {' '}
+              {data[0].name.common}
+            </Heading>
+            <div className='left-container'>
+              <div className='left-sideName'>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Native Name:</Text>
+                  <Text ml='2'>
+                    {
+                      data[0].name.nativeName[
+                        Object.keys(data[0].name.nativeName)[0]
+                      ].common
+                    }
+                  </Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Population:</Text>
+                  <Text ml='2'>{data[0].population}</Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Region:</Text>
+                  <Text ml='2'>{data[0].region}</Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Sub Region:</Text>
+                  <Text ml='2'>{data[0].subregion}</Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Capital:</Text>
+                  <Text ml='2'>{data[0].capital}</Text>
+                </Flex>
+              </div>
+              <div className='right-sideName'>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Top Level Domain:</Text>
+                  <Text ml='2'>{data[0].demonyms.eng?.m}</Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Currencies:</Text>
+                  <Text ml='2'>
+                    {
+                      data[0].currencies[Object.keys(data[0].currencies)[0]]
+                        .name
+                    }
+                  </Text>
+                </Flex>
+                <Flex alignItems='baseline'>
+                  <Text fontWeight='bold'>Languages:</Text>
+                  <Text ml='2'>
+                    {Object.values(data[0].languages).join(', ')}
+                  </Text>
+                </Flex>
+              </div>
+            </div>
+
+            <Flex marginTop='40px'>
+              {data[0].borders && (
+                <Text fontWeight='bold'>Border countries: </Text>
+              )}
+              <Flex ml='4' flexWrap='wrap' gap='10px'>
+                {data[0].borders &&
+                  data[0].borders.map((border: string) => (
+                    <Box
+                      borderRadius='md'
+                      key={border}
+                      boxShadow='md'
+                      w='78px'
+                      h='36px'
+                      paddingTop='5px'
+                      paddingBottom='5px'
+                      paddingLeft='20px'
+                      paddingRight='20px'
+                    >
+                      {border}
+                    </Box>
+                  ))}
+              </Flex>
+            </Flex>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
